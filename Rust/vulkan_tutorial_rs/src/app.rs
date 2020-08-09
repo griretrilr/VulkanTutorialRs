4734f034a8bb76bf9935bfbe8de8c933dc08b382
Grire Trilr <griretrilr@gmail.com>
2020-08-09 22:11:10 +1000

    - validation layer

--- Rust/vulkan_tutorial_rs/src/app.rs
+++ Rust/vulkan_tutorial_rs/src/app.rs
@@ -1,4 +1,4 @@
-use vulkano::instance::Instance;
+use vulkano::instance::{layers_list, Instance};
 use vulkano::swapchain::Surface;
 
 use vulkano_win::VkSurfaceBuild;
@@ -8,6 +8,13 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
 
 use std::sync::Arc;
+
+const VALIDATION_LAYERS: &[&str] = &["VK_LAYER_LUNARG_standard_validation"];
+
+#[cfg(all(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = true;
+#[cfg(not(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = false;
 
 pub struct App {
     instance: Arc<Instance>,
@@ -23,7 +30,19 @@ impl App {
         println!("Supported extensions: {:?}", supported_extensions);
         let required_extensions = vulkano_win::required_extensions();
         println!("Required extensions: {:?}", required_extensions);
-        let instance = Instance::new(Some(&app_info), &required_extensions, None).unwrap();
+
+        let instance = if ENABLE_VALIDATION_LAYERS && Self::check_validation_layer_support() {
+            Instance::new(
+                Some(&app_info),
+                &required_extensions,
+                VALIDATION_LAYERS.iter().cloned(),
+            )
+            .expect("failed to create Vulkan instance")
+        } else {
+            println!("Validation layers requested, but not available!");
+            Instance::new(Some(&app_info), &required_extensions, None)
+                .expect("failed to create Vulkan instance")
+        };
         let event_loop = EventLoop::new();
         let surface = WindowBuilder::new()
             .build_vk_surface(&event_loop, instance.clone())
@@ -34,6 +53,16 @@ impl App {
             event_loop,
             surface,
         }
+    }
+
+    fn check_validation_layer_support() -> bool {
+        let layers: Vec<_> = layers_list()
+            .unwrap()
+            .map(|l| l.name().to_owned())
+            .collect();
+        VALIDATION_LAYERS
+            .iter()
+            .all(|layer_name| layers.contains(&layer_name.to_string()))
     }
 
     pub fn run(self) {
