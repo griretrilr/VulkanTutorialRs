c57aef9231414a0ee4652ce0a31da7250b85d04e
Grire Trilr <griretrilr@gmail.com>
2020-08-10 22:50:40 +1000

    - add debug_callback

--- Rust/vulkan_tutorial_rs/src/app.rs
+++ Rust/vulkan_tutorial_rs/src/app.rs
@@ -1,4 +1,6 @@
-use vulkano::instance::{layers_list, Instance};
+use vulkano::instance::debug::{DebugCallback, MessageSeverity, MessageType};
+use vulkano::instance::ApplicationInfo;
+use vulkano::instance::{Instance, InstanceExtensions};
 use vulkano::swapchain::Surface;
 
 use vulkano_win::VkSurfaceBuild;
@@ -18,17 +20,17 @@ const ENABLE_VALIDATION_LAYERS: bool = false;
 
 pub struct App {
     instance: Arc<Instance>,
+    debug_callback: Option<DebugCallback>,
     event_loop: EventLoop<()>,
     surface: Arc<Surface<Window>>,
 }
 
 impl App {
     pub fn new() -> App {
-        let app_info = app_info_from_cargo_toml!();
-        let supported_extensions =
-            vulkano::instance::InstanceExtensions::supported_by_core().unwrap();
+        let app_info = Self::create_app_info();
+        let supported_extensions = Self::supported_extensions();
         println!("Supported extensions: {:?}", supported_extensions);
-        let required_extensions = vulkano_win::required_extensions();
+        let required_extensions = Self::required_extensions();
         println!("Required extensions: {:?}", required_extensions);
 
         let instance = if ENABLE_VALIDATION_LAYERS && Self::check_validation_layer_support() {
@@ -43,6 +45,7 @@ impl App {
             Instance::new(Some(&app_info), &required_extensions, None)
                 .expect("failed to create Vulkan instance")
         };
+        let debug_callback = Self::setup_debug_callback(&instance);
         let event_loop = EventLoop::new();
         let surface = WindowBuilder::new()
             .build_vk_surface(&event_loop, instance.clone())
@@ -50,13 +53,54 @@ impl App {
 
         App {
             instance,
+            debug_callback,
             event_loop,
             surface,
         }
     }
 
+    fn create_app_info() -> ApplicationInfo<'static> {
+        app_info_from_cargo_toml!()
+    }
+
+    fn supported_extensions() -> InstanceExtensions {
+        InstanceExtensions::supported_by_core().unwrap()
+    }
+
+    fn required_extensions() -> InstanceExtensions {
+        let mut extensions = vulkano_win::required_extensions();
+        if ENABLE_VALIDATION_LAYERS {
+            extensions.ext_debug_utils = true;
+        }
+        extensions
+    }
+
+    fn setup_debug_callback(instance: &Arc<Instance>) -> Option<DebugCallback> {
+        if !ENABLE_VALIDATION_LAYERS {
+            return None;
+        }
+
+        let msg_types = MessageType {
+            general: true,
+            validation: true,
+            performance: true,
+        }; // TODO - add a MessageType::all()
+
+        let msg_severities = MessageSeverity {
+            error: true,
+            warning: true,
+            information: true,
+            verbose: true,
+        }; // TODO - add a MessageSeverity::all()
+
+        DebugCallback::new(instance, msg_severities, msg_types, |msg| {
+            println!("debug callback: {:?}", msg.description);
+        })
+        .ok()
+    }
+
     fn check_validation_layer_support() -> bool {
-        let layers: Vec<_> = layers_list()
+        let layers: Vec<_> = vulkano::instance::layers_list()
             .unwrap()
             .map(|l| l.name().to_owned())
             .collect();
